6408a04b6f6b9bc0257460bd04ca47d14253f13f
coddinggilm <coddinggilm@gmail.com>
2021-03-10 20:49:05 +0000

    added project creation

--- routes/projects.js
+++ routes/projects.js
@@ -9,32 +9,56 @@ router.get('/', (req, res) => Project.findAll()
     .then(projects => {
         // console.log(projects)
         res.json(projects)
-        res.sendStatus(200)
+        // res.sendStatus(200)
     })
     .catch(err => console.log(err))
 );
 
+//Get 
     
 // Add a project
-router.post('/add', (req, res) => {
+router.post('/', (req, res) => {
     // const data = {
     //     title: 'Another Test Project',
     //     description: 'this is the second test'
     // }
+    // console.log(req.body)
     const data = {
-        title: req.title,
-        description: req.description
+        title: req.body.title,
+        description: req.body.description
     }
 
-    // let { title, description } = data;
+    // // Validation
+    // let errors = [];
+    // if (!title) {
+    //     errors.push({text: 'Please add a title'})
+    // }
+    // if (!description) {
+    //     errors.push({text: 'Please add a description'})
+    // }
+
+    // // Error check
+    // if (errors.length > 0) {
+
+    // } else {
+    //     Project.create({
+    //         title: data.title,
+    //         description: data.description
+    //     })
+    //     // .then(project => res.redirect('/projects'))
+    //     .then(res.json('added a project successfully'))
+    //     .catch(err => console.log(err))
+    // }
 
     Project.create({
         title: data.title,
         description: data.description
     })
-    .then(project => res.redirect('/projects'))
+    // .then(project => res.redirect('/projects'))
+    .then(res.json('added a project successfully'))
     .catch(err => console.log(err))
+
 });
-    
+      
     
 module.exports = router;
