c818df9b1f54e8e6ddf5d5afa0a07fc3a17e04c0
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-12 21:03:32 +0100

    added projects controller

---
+++ controllers/projects.js
@@ -0,0 +1,148 @@
+const fetchAllProjects = (req, res, Project) => {
+    Project.findAll({
+        where: { companyId: req.params.companyId }
+    })
+    .then(projects => res.json(projects))
+    .catch(err => console.log(err))
+};
+
+
+const fetchUserProjects = (req, res, ProjectUser) => {
+    ProjectUser.findAll({
+        where: { userID: req.params.userId }
+    })
+    .then(project => res.json(project))
+    .catch(err => console.log(err))
+};
+
+
+const fetchProject = (req, res, Project) => {
+    Project.findAll({
+        where: { id: req.params.id }
+    })
+    .then(project => res.json(project))
+    .catch(err => console.log(err))
+};
+
+
+const createProject = (req, res, Project) => {
+    Project.create({
+        title: req.body.title,
+        description: req.body.description,
+        company: req.body.company,
+        companyId: req.body.companyId
+    })
+    .then(console.log(req.body))
+    .then(res.json('added a new project successfully'))
+    .catch(err => console.log(err))
+};
+
+
+const createWithUser = (req, res, Project, ProjectUser) => {
+    Project.create({
+        title: req.body.title,
+        description: req.body.description,
+        company: req.body.company,
+        companyId: req.body.companyId
+    })
+    .then(project => ProjectUser.create({
+        userID: req.body.userId,
+        username: req.body.userAdded,
+        role: req.body.role,
+        projectID: project.id,
+        project: project.title,
+        company: req.body.company,
+        companyId: req.body.companyId 
+    }))
+    .then(console.log(req.body))
+    .then(res.json('added a new project successfully'))
+    .catch(err => console.log(err))
+};
+
+
+const updateProject = (req, res, Project, Ticket, ProjectUser) => {
+    Project.update({
+        title: req.body.title,
+        description: req.body.description
+    }, 
+    {
+        where: { id: req.params.id }
+    })
+    .then(Ticket.update(
+        { project: req.body.title },
+        {
+            where: { projectId: req.params.id }
+        }
+    ))
+    .then(ProjectUser.update(
+        { project: req.body.title },
+        {
+            where: { projectID: req.params.id }
+        }
+    )) 
+    .then(console.log(req.body))
+    .then(res.json(console.log('project successfully updated')))
+    .catch(err => console.log(err)) 
+};
+
+
+const updateAndRemoveUser = (req, res, Project, Ticket, ProjectUser) => {
+    Project.update({
+            title: req.body.title,
+            description: req.body.description
+        }, 
+        {   where: { id: req.params.id } 
+    })
+    .then(Ticket.update(
+        { project: req.body.title }, 
+        {
+            where: { projectId: req.params.id }
+        }
+    )) 
+    .then(ProjectUser.destroy({
+        where: {
+            userID: req.body.userToRemove,
+            projectID: req.params.id
+        }
+    })) 
+    .then(console.log(req.body))
+    .catch(err => console.log(err)) 
+    .then(res.json(console.log('project successfully updated and user removed')))
+};
+
+
+const removeUser = (req, res, ProjectUser) => {
+    ProjectUser.destroy({
+        where: {
+            projectID: req.params.id,
+            userID: req.body.userId
+        }
+    })
+    .then(console.log(req.body))
+    .catch(err => console.log(err)) 
+    .then(res.json(console.log('user successfully removed removed')))
+};
+
+
+const deleteProject = (req, res, Project) => {
+    Project.destroy({
+        where: { id: req.params.id }
+    })
+    
+    .then(console.log(req.body.data))
+    .then(res.json(console.log('project and all associated tickets have been successfully deleted')))
+    .catch(err => console.log(err))
+};
+
+
+module.exports = {
+    fetchAllProjects: fetchAllProjects,
+    fetchUserProjects: fetchUserProjects,
+    fetchProject: fetchProject,
+    createProject: createProject,
+    createWithUser: createWithUser,
+    updateProject: updateProject,
+    updateAndRemoveUser: updateAndRemoveUser,
+    removeUser: removeUser,
+    deleteProject: deleteProject
+};
--- routes/projects.js
+++ routes/projects.js
@@ -4,157 +4,45 @@ const db = require('../config/database');
 const Project = require('../models/Project');
 const Ticket = require('../models/Ticket');
 const User = require('../models/User');
-const ProjectUser = require('../models/ProjectUser')
+const ProjectUser = require('../models/ProjectUser');
+const projects = require('../controllers/projects');
 
 
 
 
-// Fetch all projects *************************************
-router.get('/all/:companyId', (req, res) => Project.findAll({
-        where: { companyId: req.params.companyId }
-    })
-    .then(projects => res.json(projects))
-    .catch(err => console.log(err))
-);
-
-// Fetch all Projects of a specific User *******************
-router.get('/user/:userId', (req, res) => {
-    
-    ProjectUser.findAll({
-        where: { userID: req.params.userId }
-    })
-    .then(project => res.json(project))
-    .catch(err => console.log(err))
-});   
-
-// Fetch specific project ***********************************
-router.get('/:id', (req, res) => Project.findAll({
-        where: { id: req.params.id }
-    })
-    .then(project => res.json(project))
-    .catch(err => console.log(err))
-);
+// Fetch all projects 
+router.get('/all/:companyId', (req, res) => projects.fetchAllProjects(req, res, Project));
 
 
-// Create a project  *******************************************************
-router.post('/create', (req, res) => Project.create(
-    {
-        title: req.body.title,
-        description: req.body.description,
-        company: req.body.company,
-        companyId: req.body.companyId
-    })
-    // .then(project => console.log(project))
-    .then(console.log(req.body))
-    .then(res.json('added a new project successfully'))
-    .catch(err => console.log(err))
-);
+// Fetch all Projects of a specific User 
+router.get('/user/:userId', (req, res) => projects.fetchUserProjects(req, res, ProjectUser));   
 
-// Create a project and assign a user  *************************************************
-router.post('/createWithUser', (req, res) => Project.create({
-        title: req.body.title,
-        description: req.body.description,
-        company: req.body.company,
-        companyId: req.body.companyId
-    })
-    .then(project => ProjectUser.create({
-        userID: req.body.userId,
-        username: req.body.userAdded,
-        role: req.body.role,
-        projectID: project.id,
-        project: project.title,
-        company: req.body.company,
-        companyId: req.body.companyId 
-    }))
-    .then(console.log(req.body))
-    .then(res.json('added a new project successfully'))
-    .catch(err => console.log(err))
-);
+// Fetch specific project 
+router.get('/:id', (req, res) => projects.fetchProject(req, res, Project));
 
-// Update a project ***********************************************************
-router.put('/update/:id', (req, res) => Project.update({
-        title: req.body.title,
-        description: req.body.description
-    }, 
-    {
-        where: { id: req.params.id }
-    })
-    .then(Ticket.update(
-        { project: req.body.title },
-        {
-            where: { projectId: req.params.id }
-        }
-    ))
-    .then(ProjectUser.update(
-        { project: req.body.title },
-        {
-            where: { projectID: req.params.id }
-        }
-    )) 
-    .then(console.log(req.body))
-    .then(res.json(console.log('project successfully updated')))
-    .catch(err => console.log(err))  
-)
+// Create a project  
+router.post('/create', (req, res) => projects.createProject(req, res, Project));
 
 
-// Update a project and remove a user from the project  ********************************************
-router.put('/updateAndRemoveUser/:id', (req, res) => Project.update(
-    {
-        title: req.body.title,
-        description: req.body.description
-    }, 
-    {
-        where: { id: req.params.id }
-    })
-    .then(Ticket.update(
-        { project: req.body.title }, 
-        {
-            where: { projectId: req.params.id }
-        }
-    )) 
-    .then(ProjectUser.destroy({
-        where: {
-            userID: req.body.userToRemove,
-            projectID: req.params.id
-        }
-    })) 
-    .then(console.log(req.body))
-    .catch(err => console.log(err)) 
-    .then(res.json(console.log('project successfully updated and user removed')))
-)
+// Create a project and assign a user  
+router.post('/createWithUser', (req, res) => projects.createWithUser(req, res, Project, ProjectUser));
 
-// Remove a User from a Project
-router.put('/remove-user/:id', (req, res) => ProjectUser.destroy({
-        where: {
-            projectID: req.params.id,
-            userID: req.body.userId
-        }
-    })
-    .then(console.log(req.body))
-    .catch(err => console.log(err)) 
-    .then(res.json(console.log('user successfully removed removed')))
-)
 
-// Delete a project ( CASCADE via foreign key) ********************************************************
-router.post('/delete/:id', (req, res) => Project.destroy({
-        where: { id: req.params.id }
-    })
-    // .then(Ticket.destroy({
-    //         where: {
-    //             projectId: req.body.id
-    //         }
-    //     }
-        
-    // ))
-    // .then(ProjectUser.destroy(
-    //     {
-    //         where: { projectID: req.body.id }
-    //     }
-    // ))
-    .then(console.log(req.body.data))
-    .then(res.json(console.log('project and all associated tickets have been successfully deleted')))
-    .catch(err => console.log(err))
-)
+// Update a project 
+router.put('/update/:id', (req, res) => projects.updateProject(req, res, Project, Ticket, ProjectUser));
+
+
+// Update a project and remove a user from the project  
+router.put('/updateAndRemoveUser/:id', (req, res) => projects.updateAndRemoveUser(req, res, Project, Ticket, ProjectUser));
+
+
+// Remove a User from a Project 
+router.put('/remove-user/:id', (req, res) => projects.removeUser(req, res, ProjectUser));
+
+
+// Delete a project ( CASCADE via foreign key) 
+router.post('/delete/:id', (req, res) => projects.deleteProject(req, res, Project));
+
 
 
 
