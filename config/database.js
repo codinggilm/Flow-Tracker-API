1518a5e4b1def8e9deefe95053887b9df9c7693c
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-14 07:22:06 +0100

    setting Heroku as server host

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+
+
 module.exports = new Sequelize('Flow-Tracker', 'postgres', 'millionpg1.', {
-    host: 'localhost',
+    host: 'https://lit-woodland-91230.herokuapp.com',
     dialect: 'postgres'
 })
