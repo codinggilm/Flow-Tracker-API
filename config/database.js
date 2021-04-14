3405ec75c78b37659d8f3d61b2a98b9327509331
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-14 07:55:46 +0100

    heroku

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 
 
 module.exports = new Sequelize('Flow-Tracker', 'postgres', 'millionpg1.', {
-    host: 'https://lit-woodland-91230.herokuapp.com/',
+    host: 'localhost',
     dialect: 'postgres'
 })
