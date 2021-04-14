ca781055425a067230b779f1824c2439b623d9c0
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-14 09:13:49 +0100

    sequelize

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize('Flow-Tracker', 'postgres', 'millionpg1.', {
     // host: 'localhost',
-    host: 'https://lit-woodland-91230.herokuapp.com',
-    port: process.env.PORT,
+    // host: 'https://lit-woodland-91230.herokuapp.com',
+    // port: process.env.PORT,
     dialect: 'postgres'
 })
