c54cebc3b61b18ad35f453c821f41ba116e8d82c
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-14 11:50:20 +0100

    Heroku db

--- config/database.js
+++ config/database.js
@@ -11,5 +11,8 @@ const { Sequelize } = require('sequelize');
 module.exports = new Sequelize('dlgd7vjafs6qr', 'zgqltsnvzskwhm', 'c2bb4b6b4160af61d79e5d86df7228424cf2ff7b10a9775829302ce599eec6fd', {
     host: 'ec2-34-225-167-77.compute-1.amazonaws.com',
     dialect: 'postgres',
-    port: '5432'
+    port: '5432',
+    dialectOptions: {
+        "ssl": {"require":true }
+    }
 })
