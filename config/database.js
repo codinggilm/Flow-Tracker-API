92f799b7140fbf435143905d75071f05a1f5cc5d
coddinggilm <54217958+codinggilm@users.noreply.github.com>
2021-04-14 11:34:14 +0100

    adding Heroku db

--- config/database.js
+++ config/database.js
@@ -3,9 +3,13 @@ const { Sequelize } = require('sequelize');
 
 
 
-module.exports = new Sequelize('Flow-Tracker', 'postgres', 'millionpg1.', {
-    // host: 'localhost',
-    // host: 'https://lit-woodland-91230.herokuapp.com',
-    // port: process.env.PORT,
-    dialect: 'postgres'
+// module.exports = new Sequelize('Flow-Tracker', 'postgres', 'millionpg1.', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// })
+
+module.exports = new Sequelize('dlgd7vjafs6qr', 'zgqltsnvzskwhm', 'c2bb4b6b4160af61d79e5d86df7228424cf2ff7b10a9775829302ce599eec6fd', {
+    host: 'ec2-34-225-167-77.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: '5432'
 })
